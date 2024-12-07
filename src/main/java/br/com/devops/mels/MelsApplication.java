186713fa0c4422963447d949e58ddaa22ae4d920
Alex <alexdesouzasilva94@gmail.com>
2024-12-07 10:59:04 -0300

    add

---
+++ src/main/java/br/com/devops/mels/MelsApplication.java
@@ -0,0 +1,13 @@
+package br.com.devops.mels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devops/mels/MelsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devops.mels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
